e5b38e35ca27380c65c6631e8b009cd8eda0066f
Holeryn <Holeryn@protonmail.com>
2026-06-10 17:24:13 +0200

    Un raggio di sole mi ha colpito e subito ho perso la vita....

--- progetto/popolazione.py
+++ progetto/popolazione.py
@@ -1,77 +1,103 @@
 import numpy as np
 import math as mh
-import matplotlib.pyplot as plt
 
 A0 = 3
 W = 401
 H = 401
 
-POPOLAZIONE = np.zeros([2,W,H])
-B = np.array([[1,1],[0,0]])         # Nascita
-D = np.array([[0,0],[1,1]])         # Morte
-S = np.array([[1,0],[0,1]])         # Sopravvivenza (a.k.a Identità nello spazio vettoriale delle matrici 2*2)
+POPOLAZIONE = np.zeros((2, W, H))
+VECCHIA = np.zeros((2, W, H))
 
-def Ai(i,j):
-    return (POPOLAZIONE[0][(i + 1)%W][j] +
-            POPOLAZIONE[0][i][(j+1)%H] +
-            POPOLAZIONE[0][(i+1)%W][(j+1)%H] +
-            POPOLAZIONE[0][(i-1)%W][j] +
-            POPOLAZIONE[0][i][(j-1)%H] +
-            POPOLAZIONE[0][(i-1)%W][(j-1)%H] +
-            POPOLAZIONE[0][(i+1)%W][(j-1)%H] +
-            POPOLAZIONE[0][(i-1)%W][(j+1)%H])
+B = np.array([[1, 1],
+              [0, 0]])
+
+D = np.array([[0, 0],
+              [1, 1]])
+
+S = np.array([[1, 0],
+              [0, 1]])
+
+
+def Ai(i, j):
+    return (
+        VECCHIA[0, (i+1) % W, j] +
+        VECCHIA[0, (i-1) % W, j] +
+        VECCHIA[0, i, (j+1) % H] +
+        VECCHIA[0, i, (j-1) % H] +
+        VECCHIA[0, (i+1) % W, (j+1) % H] +
+        VECCHIA[0, (i+1) % W, (j-1) % H] +
+        VECCHIA[0, (i-1) % W, (j+1) % H] +
+        VECCHIA[0, (i-1) % W, (j-1) % H]
+    )
+
 
 def V(giorno):
     if giorno <= 23:
         return 1
-    elif giorno > 23 and giorno <= 115:
+    elif giorno <= 115:
         return 0.85
-    elif giorno > 115 and giorno <= 126:
-        return 0.009*giorno - 0.185
-    elif giorno > 126 and giorno <= 156:
-        return 0.002*giorno + 0.688
-    elif giorno > 156 and giorno <= 186:
-        return 0.007*giorno - 0.092
-    else: 
+    elif giorno <= 126:
+        return 0.009 * giorno - 0.185
+    elif giorno <= 156:
+        return 0.002 * giorno + 0.688
+    elif giorno <= 186:
+        return 0.007 * giorno - 0.092
+    else:
         return 1.2
 
-def G(i,j,giorno):
-    A = Ai(i,j)
 
-    if A <= (A0 - 2*V(giorno)):
+def G(i, j, giorno):
+
+    A = Ai(i, j)
+    v = V(giorno)
+
+    if A <= A0 - 2*v:
         return D
-    elif (A > (A0-2*V(giorno))) and (A <= A0 - V(giorno)):
-        return (mh.sqrt(2) + 1)*(A0 - V(giorno) - A)*D + (A - A0 + 2*V(giorno))*S
-    elif (A > (A0 - V(giorno))) and (A <= A0):
-        return (mh.sqrt(2) + 1)*(A0 - A)*S + (A-A0+V(giorno))*B
-    elif A >= A0 and (A <= (A0 + V(giorno))):
-        return (mh.sqrt(2) + 1)*(A0 + V(giorno) - A)*B + (A - A0)*D
-    elif A >= (A0 + V(giorno)):
+
+    elif A <= A0 - v:
+        return (
+            (mh.sqrt(2)+1)*(A0-v-A)*D +
+            (A-A0+2*v)*S
+        )
+
+    elif A <= A0:
+        return (
+            (mh.sqrt(2)+1)*(A0-A)*S +
+            (A-A0+v)*B
+        )
+
+    elif A < A0 + v:
+        return (
+            (mh.sqrt(2)+1)*(A0+v-A)*B +
+            (A-A0)*D
+        )
+
+    else:
         return D
-    else: 
-        return S                # Ritorna stato corrente
 
 
 def esperimento(giorni):
-    POPOLAZIONE[0][:][:] = 1
+
+    global VECCHIA
+
+    POPOLAZIONE[0] = np.random.rand(W, H)
+    POPOLAZIONE[1] = 1.0 - POPOLAZIONE[0]
 
     for g in range(giorni):
+
+        VECCHIA = POPOLAZIONE.copy()
+
         for i in range(W):
             for j in range(H):
-                U = G(i,j,g)
-                [POPOLAZIONE[0][i][j],POPOLAZIONE[1][i][j]] = U@np.array([POPOLAZIONE[0][i][j],POPOLAZIONE[1][i][j]])
-    
+
+                U = G(i, j, g)
+
+                POPOLAZIONE[:, i, j] = U @ VECCHIA[:, i, j]
+
+                # normalizzazione probabilistica
+                s = mh.sqrt(POPOLAZIONE[0, i, j]**2 + POPOLAZIONE[1, i, j]**2)
+
+                if s > 0:
+                    POPOLAZIONE[:, i, j] /= s
+
     return POPOLAZIONE
-
-esperimento(15)
-"""
-X = np.linspace(0,190,100)
-Y = np.zeros(len(X))
-i = 0
-for x in X:
-    Y[i] = V(x)
-    i = i + 1
-
-plt.plot(X,Y)
-plt.show()
-"""
---
+++ progetto/virus.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math as mh
+import matplotlib.pyplot as plt
+
+s = 0.341
+Vp = 0.23
+Ve = 3
+Vl = 13
+Dv = 52
+Vm = 950
+Vs = 5
+
+VIRUS = np.array([401,401])
