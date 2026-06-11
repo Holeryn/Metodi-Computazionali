deb89be075762523d49d4c741a74104f6261c57a
Holeryn <Holeryn@protonmail.com>
2026-06-11 16:39:34 +0200

    devo morire, devo assolutamente morire

--- progetto/virus.py
+++ progetto/virus.py
@@ -6,12 +6,12 @@ s = 0.341
 Vp = 0.23
 Ve = 3
 Vl = 13
-Dv = 52         # A noi è diverso????
+Dv = 52
 Vm = 950
 Vs = 5
 
-def Xi(VECCHIA,VIRUS_MASK):
-    V = VECCHIA
+def Xi(VECCHIA, VIRUS_MASK):
+    V  = VECCHIA
     VM = VIRUS_MASK
 
     return (
@@ -25,40 +25,47 @@ def Xi(VECCHIA,VIRUS_MASK):
         np.roll(np.roll(V, -1, axis=0), -1, axis=1) * np.roll(np.roll(VM, -1, axis=0), -1, axis=1)
     )
 
-def Setup(POPOLAZIONE,STORICO,giorno):
-    VIRUS = np.zeros([401,401])         #3D: 1 : contagioso , 0: Non contagioso
-                                        #4D: giorno in cui inizia il contagio
-    VIRUS_MASK = np.zeros([401,401])
-    GIORNO_MASK = np.full([401,401],-999)
-    MASK = np.zeros([401,401])
-    POP_MASK = np.zeros([401,401])
-                                               
+def Setup(POPOLAZIONE, STORICO, giorno):
+    VIRUS       = np.zeros((401, 401))
+    VIRUS_MASK  = np.zeros((401, 401))
+    GIORNO_MASK = np.full((401, 401), -999)  # -999 = mai infettato
+    MASK        = np.zeros((401, 401))
+    POP_MASK    = np.full((401, 401), 3)     # contatore 3 giorni popolazione
 
-    if(giorno < Dv):
+    if giorno < Dv:
         return VIRUS
-    
-    VIRUS[::40, ::40] = 1                   # GIORNO 52(DV) Aggiungi 1 ogni 1600
-    VIRUS_MASK[::40,::40] = 0               # Non cotagioso
-    GIORNO_MASK[::40,::40] = Dv             # Nace a Dv
-    MASK[::40,::40] = 1                     # Nom modificare più
-    POP_MASK[::40,::40] = 3
-    
-    for i in range(Dv + 1, giorno-2):
-        X_i = Xi(VIRUS,VIRUS_MASK)
-        N = Vp*X_i*POPOLAZIONE[0]
+
+    # Regola 1: pianta virus al giorno Dv
+    VIRUS[::40, ::40]       = 1
+    GIORNO_MASK[::40, ::40] = Dv
+    MASK[::40, ::40]        = 1
+
+    for i in range(Dv + 1, giorno - 2):
+
+        # Regola 2: propaga virus
+        X_i   = Xi(VIRUS, VIRUS_MASK)
+        N     = Vp * X_i * STORICO[i]
         VIRUS = VIRUS + N
-        GIORNO_MASK = np.where((VIRUS != 0) & (MASK == 0),i,GIORNO_MASK)
-        VIRUS_MASK = np.where((i > (GIORNO_MASK + Ve + 1)) & (i < (GIORNO_MASK + Ve + Vl + 1)),1,0)
-        VIRUS = np.where(VIRUS_MASK == 0, 0,VIRUS)
-        MASK = np.where((VIRUS != 0) & (MASK == 0), 1, MASK)
 
-        POP_MASK = np.where((STORICO[i] == 0) & (POP_MASK >= 0),POP_MASK - 1,POP_MASK)
-        VIRUS = np.where(POP_MASK != 0, VIRUS,0)
+        # Segna giorno di nascita per le celle appena infettate
+        GIORNO_MASK = np.where((VIRUS != 0) & (MASK == 0), i, GIORNO_MASK)
+        MASK        = np.where((VIRUS != 0) & (MASK == 0), 1, MASK)
 
+        # Regola 3: calcola quanti giorni è vivo il virus in ogni cella
+        giorni_vita = i - GIORNO_MASK
 
+        # Infettivo solo nella finestra (Ve, Ve+Vl]
+        VIRUS_MASK = np.where(
+            (giorni_vita > Ve) & (giorni_vita <= Ve + Vl),
+            1, 0
+        )
 
+        # Rimuovi virus solo quando ha superato tutta la sua vita (Ve + Vl)
+        VIRUS = np.where(giorni_vita > Ve + Vl, 0, VIRUS)
 
+        # Regola 4: se popolazione = 0 per 3 giorni consecutivi, azzera virus
+        POP_MASK = np.where(STORICO[i] == 0, POP_MASK - 1, 3)
+        POP_MASK = np.clip(POP_MASK, 0, 3)
+        VIRUS    = np.where(POP_MASK == 0, 0, VIRUS)
 
-          
-
-
+    return VIRUS
