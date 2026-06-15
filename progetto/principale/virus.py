9a17bdd15e09c43efaa0523ce13975251cf2f35b
Holeryn <holeryn@debian>
2026-06-16 00:53:17 +0200

    Gli spettri si nascondono sotto i miei occhi e si lietano delle mie sventure

---
+++ progetto/principale/virus.py
@@ -0,0 +1,78 @@
+import numpy as np
+import math as mh
+import matplotlib.pyplot as plt
+import random
+from scipy.special import gamma
+
+s = 0.341
+Vp = 0.23
+Ve = 3
+Vl = 13
+Dv = 40
+Vm = 950
+Vs = 5
+
+# Parametri P(x)
+alpha = 4.938
+beta = 0.2627
+
+def Xi(VIRUS):
+    V = VIRUS
+
+    # Ora facciamo i roll solo sulla matrice già filtrata
+    return (
+        # 4 Primi vicini (Cardinali)
+        np.roll(V,  1, axis=0) +
+        np.roll(V, -1, axis=0) +
+        np.roll(V,  1, axis=1) +
+        np.roll(V, -1, axis=1) +
+        
+        # 4 Vicini diagonali (Rimuovili se volevi SOLO i primi 4)
+        np.roll(np.roll(V,  1, axis=0),  1, axis=1) +
+        np.roll(np.roll(V,  1, axis=0), -1, axis=1) +
+        np.roll(np.roll(V, -1, axis=0),  1, axis=1) +
+        np.roll(np.roll(V, -1, axis=0), -1, axis=1)
+    )
+
+
+def setup_virus(giorni_totali, STORICO):
+    VIRUS_LAYERS  = np.zeros((giorni_totali, 401, 401))
+    N_LAYERS      = np.zeros((giorni_totali,401,401))  # N per cella per giorno
+    
+    indici = np.random.choice(401*401, size=100, replace=False)
+    VIRUS_LAYERS[Dv].flat[indici] = 1
+    
+    POP_ZERO = np.zeros((401, 401), dtype=int)
+
+    for i in range(Dv + 1, giorni_totali):
+        if i % Vs == 0:
+            indici_nuovi = np.random.choice(401*401, size=1, replace=False)
+            VIRUS_LAYERS[i].flat[indici_nuovi] = 1
+
+        eta           = i - np.arange(i)[:, np.newaxis, np.newaxis]
+        infettivi_mask = (eta > Ve) & (eta <= Ve + Vl)
+        VIRUS_INFETTI  = (VIRUS_LAYERS[:i] * infettivi_mask).sum(axis=0)
+        Xi_val         = Xi(VIRUS_INFETTI)
+        
+        N                = Vp * Xi_val * STORICO[i]  # (401,401)
+        N_LAYERS[i]      = N                          # salva per cella
+        VIRUS_LAYERS[i] += N
+
+        totale = VIRUS_LAYERS[:i+1].sum(axis=0)
+        eccesso_mask = totale > Vm
+        if eccesso_mask.any():
+            scala = np.where(eccesso_mask, Vm / totale, 1.0)
+            VIRUS_LAYERS[:i+1] *= scala[np.newaxis, :, :]
+
+        eta_full = i - np.arange(i+1)[:, np.newaxis, np.newaxis]
+        VIRUS_LAYERS[:i+1] = np.where(eta_full > Ve + Vl, 0, VIRUS_LAYERS[:i+1])
+
+        POP_ZERO = np.where(STORICO[i] == 0, POP_ZERO + 1, 0)
+        maschera_morte = POP_ZERO >= 3
+        VIRUS_LAYERS[i][maschera_morte] = 0
+        N[maschera_morte] = 0
+
+    return N_LAYERS, VIRUS_LAYERS
+
+def P(x):
+    return (beta**alpha)/gamma(alpha) * x**(alpha-1) * np.exp(-beta*x)
